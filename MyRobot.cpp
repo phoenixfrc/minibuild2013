f7a8559c2ce7569388435553d4cc6ebace18cc44
Phoenix1 team machine <phoenix1@team2342.org>
2013-12-02 18:46:26 -0500

    checkpoint code - basic camera working now. debugging kicker still

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -13,7 +13,13 @@ class RobotDemo : public SimpleRobot
 	Joystick rightStick;
 	Joystick gamePad;
 	DriverStationLCD *lcd;
-
+	Servo cameraPivotMotor;
+	Servo cameraElevateMotor;
+	float cameraElevateAngle;
+	float cameraPivotAngle;
+	Compressor compressor;
+	Relay kicker;
+	Relay blocker;
 public:
 	RobotDemo(void):
 		myRobot(1, 2, 3, 4),	// these must be initialized in the same order
@@ -21,11 +27,23 @@ public:
 		leftStick(2),		// as they are declared above.
 		rightStick(1),
 		gamePad(3),
-		lcd(DriverStationLCD::GetInstance())
+		lcd(DriverStationLCD::GetInstance()),
+		cameraPivotMotor(9),
+		cameraElevateMotor(10),
+		compressor(9,1),
+		kicker(2),
+		blocker(3)
 	{
 		myRobot.SetExpiration(0.1);
+		compressor.Start();
 	}
-
+	void RobotInit(){
+		cameraElevateAngle =
+							(cameraElevateMotor.GetMaxAngle()-cameraElevateMotor.GetMinAngle()) * 2/3;
+		cameraPivotAngle = 0;
+		cameraPivotMotor.SetAngle(cameraPivotAngle);
+		cameraElevateMotor.SetAngle(cameraElevateAngle);
+	}
 	/**
 	 * Drive left & right motors for 2 seconds then stop
 	 */
@@ -43,7 +61,7 @@ public:
 	void OperatorControl(void)
 	{
 		myRobot.SetSafetyEnabled(true);
-		lcd->PrintfLine(DriverStationLCD::kUser_Line5, "Hi there from phoenix2");
+		lcd->PrintfLine(DriverStationLCD::kUser_Line5, "Hi there from phoenix1");
 		lcd->PrintfLine(DriverStationLCD::kUser_Line1, "%s", "in OpCtl - rev 1");
 		lcd->UpdateLCD();
 		int loopcount = 0;
@@ -58,6 +76,32 @@ public:
 			}
 			
 			myRobot.TankDrive(leftStick, rightStick); // drive with arcade style (use right stick)
+			if (gamePad.GetY() > 0.05 ||
+					gamePad.GetY() < -0.05) {
+				cameraElevateAngle += gamePad.GetY();
+				if (cameraElevateAngle < cameraElevateMotor.GetMinAngle())
+					cameraElevateAngle = cameraElevateMotor.GetMinAngle();
+				if (cameraElevateAngle > cameraElevateMotor.GetMaxAngle())
+					cameraElevateAngle = cameraElevateMotor.GetMaxAngle();
+			}
+			if (gamePad.GetX() > 0.05 ||
+					gamePad.GetX() < -0.05) {
+				cameraPivotAngle += gamePad.GetX();
+				if (cameraPivotAngle < cameraPivotMotor.GetMinAngle())
+					cameraPivotAngle = cameraPivotMotor.GetMinAngle();
+				if (cameraPivotAngle > cameraPivotMotor.GetMaxAngle())
+					cameraPivotAngle = cameraPivotMotor.GetMaxAngle();
+			}
+			cameraPivotMotor.SetAngle(cameraPivotAngle);
+			cameraElevateMotor.SetAngle(cameraElevateAngle);
+			if (gamePad.GetRawButton(8)){
+				kicker.Set(Relay::kReverse);
+				blocker.Set(Relay::kReverse);
+			}
+			else{
+				kicker.Set(Relay::kOff);
+				blocker.Set(Relay::kOff);
+			}
 			Wait(0.005);				// wait for a motor update time
 			loopcount++;
 		}
